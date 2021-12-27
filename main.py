fca46e5a724d6c933d3a680a2377807dde9cd08d
Matt Gleich <git@mattglei.ch>
2021-12-26 23:02:34 -0500

    feat: calc time diff
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import os
 from pathlib import Path
 from typing import Any, MutableMapping
-from loguru import logger
+from datetime import datetime, timedelta
+
 import toml
+from loguru import logger
 
 
 def main() -> None:
     """Main function for the program"""
-    print(config())
+    conf = config()
+    diff = calculated_time(conf)
+    print(diff)
 
 
 def config() -> MutableMapping[str, Any]:
@@ -23,6 +27,11 @@ def config() -> MutableMapping[str, Any]:
         config = toml.load(config_file)
         logger.success("Loaded config")
         return config
+
+
+def calculated_time(config: MutableMapping[str, Any]) -> timedelta:
+    now = datetime.now()
+    return datetime.combine(config["date"], datetime.min.time()) - now
 
 
 if __name__ == "__main__":
