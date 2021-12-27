57cc37ee57709abc2c42623e4e6dd66615ea4b7b
Matt Gleich <git@mattglei.ch>
2021-12-26 22:36:24 -0500

    feat: load config file
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+import os
+from pathlib import Path
+from typing import Any, MutableMapping
+from loguru import logger
+import toml
+
+
 def main() -> None:
-    print("Hello World!")
+    """Main function for the program"""
+    print(config())
+
+
+def config() -> MutableMapping[str, Any]:
+    """Load the config file for oled_countdown
+
+    Returns:
+        MutableMapping[str, Any]: The loaded TOML file
+    """
+    config_location = os.path.join(
+        Path.home(), ".config", "oled_countdown", "config.toml"
+    )
+    with open(config_location) as config_file:
+        config = toml.load(config_file)
+        logger.success("Loaded config")
+        return config
 
 
 if __name__ == "__main__":
